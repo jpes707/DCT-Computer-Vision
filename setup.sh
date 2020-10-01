909297b28ba8ec9fd63928863bf7e1bbc8809e8b
Vineel Vanam <vineel.vanam@gmail.com>
2020-10-01 01:27:27 -0700

    setup file for linux

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo snap install pycharm-professional --classic
+sudo apt install python3.8
+sudo apt install python3-pip
+pip3 install opencv-python
+sudo apt-get install cmake
+pip3 install apriltag
+pip3 install matplotlib
+sudo apt-get install git
+read -p "Link to forked git repository? " gitrepo
+git clone $gitrepo
