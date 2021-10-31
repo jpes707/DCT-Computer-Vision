c170dc07542c6b7700a468e53223cb90af9f9c39
Johnny Pesavento <jpes707@gmail.com>
2021-10-31 14:33:15 -0400

    Added divisions code

--- test.py
+++ test.py
@@ -2,17 +2,26 @@ import apriltag
 import cv2
 import os
 import numpy as np
+from time import time
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # slide a window across the image
+start_time = time()
 def reader(detector, img, file_name):
+    print(img.shape)
     tag_list = []
     global img2
-    for y in range(0, img.shape[0], 350):
-        for x in range(0, img.shape[1], 350):
-            result, img2 = detector.detect(img[y:y + 450, x:x + 450], return_image=True)
-            if (len(result) != 0) and all(result) not in tag_list:
-                tag_list.append(result)
-                cv2.imwrite("Outputs/"+file_name+".output.jpg", img2)
+    divisions = 12
+    success = False
+    while not success and divisions < 150:
+        print(divisions)
+        for y in range(0, img.shape[0], img.shape[0] // divisions):
+            for x in range(0, img.shape[1], img.shape[1] // divisions):
+                result, img2 = detector.detect(img[y:y + 325, x:x + 325], return_image=True)
+                if (len(result) != 0) and all(result) not in tag_list:
+                    tag_list.append(result)
+                    cv2.imwrite("Outputs/"+file_name+".output.jpg", img2)
+                    success = True
+        divisions += 24
     return tag_list
 if __name__ == '__main__':
     fail = []
@@ -26,5 +35,6 @@ if __name__ == '__main__':
         if(len(tag_list)==0):
             fail.append(file)
         print(tag_list)
+    print('time', time() - start_time)
     cv2.destroyAllWindows()
     print(fail)
